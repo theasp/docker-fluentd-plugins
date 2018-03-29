24544b2bbd2727f70a43d6451a3f6c2d0d9e8952
Andrew Phillips <theasp@gmail.com>
2018-03-29 09:46:59 -0300

    Use a single build script for all images

---
+++ build.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -exu
+set -o pipefail
+
+BUILD_DEPS="jq curl make gcc g++ rake patch libc-dev ruby-dev zlib1g-dev libcurl4-openssl-dev libpq-dev libssl-dev libsqlite3-dev default-libmysqlclient-dev libgeoip-dev libsasl2-dev libxml2-dev libffi-dev libmaxminddb-dev ruby-aws-sdk"
+JQ=''
+SKIP='^$'
+ALLOW_FAIL=false
+
+sed -r -i -e 's! main! main contrib non-free!g' /etc/apt/sources.list
+
+apt-get update -q
+apt-get install -qy --no-install-recommends $BUILD_DEPS
+
+case $1 in
+  certified) JQ='map(select(.obsolete != true and (.certified|length) > 0)) | .[].name | @text' ;;
+  slim)      JQ='map(select(.obsolete != true and (.downloads > 20000 or (.certified|length) > 0))) | .[].name | @text' ;;
+  common)    JQ='map(select(.obsolete != true and (.downloads > 5000 or (.certified|length) > 0))) | .[].name | @text'
+             SKIP='^fluent-plugin-(chef-client|grassland|mysql-binlog|monolog)$' ;;
+  all)       JQ='map(select(.obsolete != true) | .[].name | @text'
+             ALLOW_FAIL=true ;;
+  *)
+    echo "Unknown build type: $1"
+    exit 1;;
+esac
+
+if ! curl -s https://www.fluentd.org/plugins \
+    | grep '^  var plugins' \
+    | cut -f 2- -d = \
+    | sed -e 's/;$//' \
+    | jq -r "$JQ" \
+    | grep -v "$SKIP" \
+    | xargs -r gem install;
+then
+  if [[ $ALLOW_FAIL != true ]]; then
+    echo "Error installing packages"
+    exit 1
+  fi
+fi
+
+gem list 'fluent-plugins*' | tee /plugins-installed
+gem sources --clear-all
+
+apt-get purge -qy $(dpkg-query --show --showformat '${Package}\n' '*-dev')
+rm -rf /var/lib/apt/lists/* /home/fluent/.gem/ruby/*/cache/*.gem
