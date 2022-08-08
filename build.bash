6ab69e77467f5e49c42efad56dde0bf01b42b29a
Andrew Phillips <theasp@gmail.com>
2022-08-08 12:12:44 -0300

    Rework build script

--- build.bash
+++ build.bash
@@ -3,44 +3,87 @@
 set -exu
 set -o pipefail
 
-BUILD_DEPS="jq curl make gcc g++ rake patch libc-dev ruby-dev zlib1g-dev libcurl4-openssl-dev libpq-dev libssl-dev libsqlite3-dev default-libmysqlclient-dev libgeoip-dev libsasl2-dev libxml2-dev libffi-dev libmaxminddb-dev ruby-aws-sdk"
+BUILD_DEPS=(jq curl make gcc g++ rake patch libc-dev ruby-dev zlib1g-dev libcurl4-openssl-dev libpq-dev libssl-dev libsqlite3-dev default-libmysqlclient-dev libgeoip-dev libsasl2-dev libxml2-dev libffi-dev libmaxminddb-dev ruby-aws-sdk)
 JQ=''
 ALLOW_FAIL=true
 SKIP='^fluent-plugin-(geoblipper|chef-client|grassland|mysql-binlog|monolog|filter-list|couchbase|splunk-hec|azure-loganalytics)$'
 
-case $1 in
-  certified) JQ='map(select(.obsolete != true and (.certified|length) > 0)) | .[].name | @text' ;;
-  slim)      JQ='map(select(.obsolete != true and (.downloads > 20000 or (.certified|length) > 0))) | .[].name | @text' ;;
-  common)    JQ='map(select(.obsolete != true and (.downloads > 5000 or (.certified|length) > 0))) | .[].name | @text' ;;
-  all)       JQ='map(select(.obsolete != true) | .[].name | @text'
-             SKIP='^$'
-             ALLOW_FAIL=true ;;
-  *)
-    echo "Unknown build type: $1"
-    exit 1 ;;
-esac
+function main {
+  local series=$1
 
-sed -r -i -e 's! main! main contrib non-free!g' /etc/apt/sources.list
+  if [[ -z $series ]]; then
+    echo "Usage: $0 <series>"
+  fi
 
-apt-get update -q
-apt-get install -qy --no-install-recommends $BUILD_DEPS
+  install_deps
+  install_plugins "${series}"
+  cleanup
+}
 
-if ! curl -s https://www.fluentd.org/plugins \
-    | grep '^  var plugins' \
-    | cut -f 2- -d = \
-    | sed -e 's/;$//' \
-    | jq -r "$JQ" \
-    | egrep -v "$SKIP" \
-    | xargs -r gem install;
-then
-  if [[ $ALLOW_FAIL != true ]]; then
-    echo "Error installing packages"
+function install_deps {
+  sed -r -i -e 's! main! main contrib non-free!g' /etc/apt/sources.list
+
+  apt-get update -q
+  apt-get install -qy --no-install-recommends "${BUILD_DEPS[@]}"
+
+  install_plugins "${series}"
+}
+
+function install_plugins {
+  local series=$1
+
+  for plugin in $(plugins_for_series "${series}"); do
+    if gem list --installed --exact "${plugin}" 1>/dev/null 2>&1; then
+      continue
+    fi
+    if ! gem install "${plugin}"; then
+      if [[ $ALLOW_FAIL != true ]]; then
+        echo "ERROR: Unsable to install package ${plugin} in series ${series}" 1>&2
+        exit 1
+      else
+        echo "WARNING: Unsable to install package ${plugin} in series ${series}" 1>&2
+      fi
+    fi
+  done
+}
+
+function plugins_for_series {
+  local series=$1
+
+  case $1 in
+    certified) JQ='map(select(.obsolete != true and (.certified|length) > 0)) | .[].name | @text' ;;
+    slim)      JQ='map(select(.obsolete != true and (.downloads > 20000 or (.certified|length) > 0))) | .[].name | @text' ;;
+    common)    JQ='map(select(.obsolete != true and (.downloads > 5000 or (.certified|length) > 0))) | .[].name | @text' ;;
+    all)       JQ='map(select(.obsolete != true) | .[].name | @text'
+               SKIP='^$'
+               ALLOW_FAIL=true ;;
+    *)
+      echo "Unknown build type: $1"
+      exit 1 ;;
+  esac
+
+
+  if ! curl -s https://www.fluentd.org/plugins \
+      | grep '^  var plugins' \
+      | cut -f 2- -d = \
+      | sed -e 's/;$//' \
+      | jq -r "$JQ" \
+      | egrep -v "$SKIP" \
+      | sort; then
+    echo "ERROR: Unable to get list of plugins" 1>&2
     exit 1
   fi
-fi
+}
 
-gem list 'fluent-plugins*' | tee /plugins-installed
-gem sources --clear-all
+function make_plugin_list {
+  gem list 'fluent-plugins*' | tee /plugins-installed
+  gem sources --clear-all
+}
 
-apt-get purge -qy $(dpkg-query --show --showformat '${Package}\n' '*-dev')
-rm -rf /var/lib/apt/lists/* /home/fluent/.gem/ruby/*/cache/*.gem
+
+function cleanup {
+  apt-get purge -qy $(dpkg-query --show --showformat '${Package}\n' '*-dev')
+  rm -rf /var/lib/apt/lists/* /home/fluent/.gem/ruby/*/cache/*.gem
+}
+
+main "$@"
